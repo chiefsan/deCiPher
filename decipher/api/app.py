baac022eb35dfc3c1034beb20cbfcea228a0ab83
Sanjay Seetharaman <sanjayms0111@gmail.com>
2020-11-20 13:48:15 +0530

    Update app.py

--- decipher/api/app.py
+++ decipher/api/app.py
@@ -188,9 +188,9 @@ def render_comments():
 
 
 if __name__ == "__main__":
-    api = Api(blueprint)
+    api = Api()
     api.add_resource(commentList, '/comments')
     api.add_resource(comment, '/comments/<comment_id>')
     app = create_app()
-    # api.init_app(app)
+    api.init_app(blueprint)
     app.run(host="127.0.0.1", port=5002, debug=True)
